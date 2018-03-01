83b3f10f7f74224098676bea46b2ad416778f0e6
Kenny Drobnack <kennydrobnack@gmail.com>
2018-03-01 06:45:55 -0500

    Finished bit about how to use .map to populate lists

--- src/App.js
+++ src/App.js
@@ -1,11 +1,73 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Kenny', age: 39 },
+      { name: 'Stephanie', age: 26}
+    ],
+    showPersons: true,
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({ showPersons: !doesShow })
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+      { name: newName, age: 28},
+      { name: 'Kenneth', age: 39 },
+      { name: 'Steph', age: 26}
+      ]})
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: 28},
+        { name: 'Kenneth', age: 39 },
+        { name: 'Steph', age: 26}
+        ]})
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+
+    let persons = null
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          { this.state.persons.map(person => {
+            return <Person name={person.name}
+              age={person.age}/>
+            })}
+        </div>
+      ) 
+    }
+
     return (
       <div className="App">
         <h1>Hi I'm a React app</h1>
+        <button
+          style={style} 
+          onClick={() => this.switchNameHandler('Maximillian!!')}>Switch names</button>
+                <button
+          style={style} 
+          onClick={() => this.togglePersonsHandler()}>Toggle persons</button>
+        { this.state.showPersons ? persons : null }
       </div>
     );
   }
