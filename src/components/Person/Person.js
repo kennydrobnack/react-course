cf91179278772117dbaff0580abf66e749acd170
Kenny Drobnack <kennydrobnack@gmail.com>
2018-03-16 06:26:17 -0400

    Added propTypes. Person.js still has props is not defined error

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import classes from './Person.css'
 import withClass from '../../hoc/withClass'
 import Aux from '../../hoc/Aux'
+import PropTypes from 'prop-types'
 
 class Person extends Component {
     constructor(props) {
@@ -10,12 +11,19 @@ class Person extends Component {
 
     render () { 
         return (
-            <Aux>
+            <div>
                 <p onClick={props.click}>I'm a {props.name} and am {props.age} years old</p>
                 <p>{props.children}</p>
                 <input type="text" onChange={props.changed} value={props.name}/>
-            </Aux>
+            </div>
     )}
 }
 
-export default withClass(Person, classes.Person)
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string, 
+    age: PropTypes.number,
+    changed: PropTypes.func
+}
+
+export default Person
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-//import classes from './App.css'
+import classes from './App.css'
 import People from '../components/People/People'
 import Cockpit from '../components/Cockpit/Cockpit'
 import Aux from '../hoc/Aux'
